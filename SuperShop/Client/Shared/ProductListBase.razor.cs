719f6a840255a65315b8a5d9bcf7da4b2edf24c7
Marcus Elcamel <conti.marcus6@gmail.com>
2022-04-21 22:02:57 +0100

    Add project files.

---
+++ SuperShop/Client/Shared/ProductListBase.razor.cs
@@ -0,0 +1,16 @@
+﻿/*using Microsoft.AspNetCore.Components;
+using SuperShop.Shared;
+
+namespace SuperShop.Client.Shared
+{
+    public class ProductListBase : ComponentBase
+    {
+        protected static List<Product> Products { get; set; } = new List<Product>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Products = await 
+        }
+    }
+}
+*/
